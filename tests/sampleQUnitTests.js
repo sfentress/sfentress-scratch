2a985ab7e9a56c70a90d455fed03a199188eea88
Sam Fentress <sfentress@dhcp135.concord.org>
2010-07-07 15:58:04 -0400

    Modify tests so they all pass in everything but FF

--- tests/sampleQUnitTests.js
+++ tests/sampleQUnitTests.js
@@ -2,11 +2,7 @@ test('simple test passes', function() {
     ok(true, 'This test should always pass');  
 });
 
-test('simple test fails', function() { 
-    ok(false, 'This test should always fail');  
-});
-
-test('this test passes only in FireFox', function() {
+test('this test passes in everything but FireFox', function() {
   var isFirefox = (navigator.userAgent.indexOf("Firefox")!=-1);
-  ok(isFirefox, 'this browser is FireFox');
+  ok(!isFirefox, 'this browser is not FireFox');
 });
